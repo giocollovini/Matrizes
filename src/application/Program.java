1457c2ffe664bfec44315371bc9c88789934afca
Giovani Collovini <gcollovini@gmail.com>
2020-03-03 08:18:36 -0300

    Projeto Trabalhando com Matrizes

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Quantidade de Linhas e colunas da Matriz: ");
+		
+		int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Main diagonal:");
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+
+		int count = 0;
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Negative numbers = " + count); 
+		
+		System.out.println("Quantity Lines[mat.length] = " + count);
+		System.out.println("Quantity Columns[mat[i].length] = " + count);
+
+		sc.close();
+
+	}
+}
